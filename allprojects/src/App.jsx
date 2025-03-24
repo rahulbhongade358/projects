0f763d6a917ec3bc1be7010cbb88e19ba9bd95dc
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-03-24 23:36:50 +0530

    set up react

---
+++ allprojects/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-blue-600 text-5xl text-white p-2.5 m-[50px] justify-center items-center flex'>Test</h1>
+    </div>
+  )
+}
+
+export default App
