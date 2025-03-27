8606232963138a4c601e09f4fc1842ce5aabc25e
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-03-27 20:08:55 +0530

    set up for BG Changer

--- allprojects/src/components/Bgchanger.jsx
+++ allprojects/src/components/Bgchanger.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Bgchanger() {
+  const [color, setcolor] = useState("burlywood")
+
   return (
-    <div>
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}>
       <h2>Background Changer</h2>
+      <button 
+      onClick={()=>(setcolor('red'))}
+      className="h-8 m-8 cursor-pointer p-2.5 border-2 bg-red-500 text-white hover:bg-red-600 transition duration-300 flex items-center justify-center rounded-lg">
+  Red
+</button>
     </div>
   )
 }
