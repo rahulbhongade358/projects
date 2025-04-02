da354fccc7fbbd763719b315a7474e04ee4a5bff
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-04-02 23:30:25 +0530

    added functionality

--- allprojects/src/components/hooks/hooks.jsx
+++ allprojects/src/components/hooks/hooks.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Hooks.css'
 function hooks() {
+  const [quantity, setquantity] = useState(0)
+
+  
+  const increase = ()=>{
+    if (quantity==6) {
+   alert(" you reached the limit ")
+    }else{setquantity(quantity+1)}
+  }
+  const decrease = ()=>{
+    if (quantity>1) {
+      setquantity(quantity-1)
+    }
+  }
   return (
     <div>
       This project based on Hook's
@@ -9,9 +22,9 @@ function hooks() {
       <img src="https://images3.alphacoders.com/104/1041781.jpg" alt="Pizza" />
       <p className='desc'>Delicious cheesy pizza with fresh toppings and a crispy crust.</p>
       <p className='price'>Price: ₹399</p>
-      <button className='plus'>-</button>
-      <p className='total'>Total: 0 </p>
-      <button className='minus'>+</button>
+      <button className='minus' onClick={decrease}>-</button>
+      <p className='total'>Total:{quantity}</p>
+      <button className='plus' onClick={increase}>+</button>
     </div>
     </div>
   )
